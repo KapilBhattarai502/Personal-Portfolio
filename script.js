73ab5bc4e352dd97e1b026cc7f7abb167d4e010e
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-06-18 23:08:57 +0545

    services

---
+++ script.js
@@ -0,0 +1,18 @@
+let tablinks =document.getElementsByClassName("tab-links");
+let tabcontents=document.getElementsByClassName("tab-contents");
+
+
+function opentab(tabname){
+    for(let tablink of tablinks){
+
+        tablink.classList.remove("active-link");
+        
+    }
+
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+        document.getElementById(tabname).classList.add("active-tab")
+    }
+//    event.target.classList.add("active-link");
+
+}
